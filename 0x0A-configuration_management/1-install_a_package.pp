4c4219dc7cd926efc7f375e0ea9cf0d44a6e4b73
G-omar-H <omarghazi12@gmail.com>
2023-12-22 21:03:12 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,19 @@
 # Puppet script manifestating to install flask from pip3
+package { 'python':
+  ensure => present,
+}
+package { 'python3-pip':
+  ensure => present,
+}
+
 package {'flask':
   ensure   => '2.1.0',
   name     => flask,
   provider => pip3,
+}
+
+package { 'Werkzeug':
+  ensure   => present,
+  provider => pip,
 }
 
