edac4f640238e9347a7f68f7cd51418850328b54
G-omar-H <omarghazi12@gmail.com>
2023-12-22 21:16:45 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,26 @@
-# Puppet script manifestating to install flask from pip3
 
-# install python 3.8.10
+# Installs flask and required packages
+
+# Install Python 3.8.10
 package { 'python3.8':
   ensure => '3.8.10',
 }
-# install pip3
+
+# Install pip
 package { 'python3-pip':
   ensure => present,
 }
-# install falsk 2.1.0
-package {'flask':
+
+# Install Flask 2.1.0
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-  require  => package['python3-pip'],
+  require  => Package['python3-pip'],
 }
 
-# install Werkzeug
-package { 'Werkzeug':
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip',
-  require  => package['python3-pip'],
+  require  => Package['python3-pip'],
 }
-
-
