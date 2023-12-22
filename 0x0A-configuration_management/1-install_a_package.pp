2a13b377ebc769ca8575e9e824fd3c4254d36b6a
G-omar-H <omarghazi12@gmail.com>
2023-12-22 21:21:42 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -14,13 +14,13 @@ package { 'python3-pip':
 # Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => pip3,
   require  => Package['python3-pip'],
 }
 
 # Install Werkzeug 2.1.1
 package { 'werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip',
+  provider => pip,
   require  => Package['python3-pip'],
 }
