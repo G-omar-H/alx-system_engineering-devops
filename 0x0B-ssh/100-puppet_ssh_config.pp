65efc62db743cd047d48ef3eda8410116f5765eb
G-omar-H <omarghazi12@gmail.com>
2024-01-01 18:25:03 +0100

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,13 @@
 # puppet script to setup client SSH configuration file to disable password authentification
 file_line { 'PasswordAuthentication':
   ensure => present,
-  path   => '~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
   line   => '       PasswordAuthentication no',
   match  -> '/.\s+PasswordAuthentication\s+yes/gm',
+}
+file_line {'IdentityFile ~/.ssh/school':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config'
+  line   => '       IdentityFile ~/.ssh/school'
+  match  => '/./s+IdentityFile/s+/'
 }
