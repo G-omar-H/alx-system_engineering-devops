89e72d79051a1b043d8f43fd8f385744a7351bf7
G-omar-H <omarghazi12@gmail.com>
2024-01-29 18:36:22 +0100

    1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Python script that, using  REST API,
+for a given employee ID,
+returns information about
+his/her TODO list progress
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import numpy as np
+    import requests
+    import sys
+
+    if __name__ == "__main__":
+        id = sys.argv[1]
+
+        url_1 = f"https://jsonplaceholder.typicode.com/users/{id}"
+
+        url_2 = "https://jsonplaceholder.typicode.com/todos"
+
+        p = requests.get(url_1)
+        content = json.loads(p.content)
+        name = content["name"]
+
+        r = requests.get(url_2)
+        content_2 = json.loads(r.content)
+
+        alist = []
+
+        for item in content_2:
+            if item["userId"] == int(id):
+                alist.append(f"({id}, {name},\
+                {item['completed']}, {item['title']}")
+
+        arr = np.array(alist)
+        np.savetxt(f"{id}.csv", arr, fmt="%s")
