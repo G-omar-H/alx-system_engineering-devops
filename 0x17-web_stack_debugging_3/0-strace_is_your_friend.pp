c920b1a268ce4e8009372d51fa1eed217e3357c1
G-omar-H <omarghazi12@gmail.com>
2024-02-17 18:02:07 +0100

    0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Puppet script to fix the Wordpress internal server error
 
-exec { 'fix the wordpress':
-  command => 'sed -r "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php'
+exec { 'sed -ir "s/class-wp-locale.phpp/class-wp-locale.php/g" wp-settings.php':
+  cwd  => '/var/www/html/',
+  path => '/bin/',
 }
